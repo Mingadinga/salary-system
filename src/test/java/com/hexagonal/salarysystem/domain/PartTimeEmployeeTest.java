690490ea10339e62862d7253099291f7ea783601
mingadinga <minpearl0826@gmail.com>
2024-01-21 12:52:44 +0900

    급여 계산/도메인/테스트 코드 변수명 수정 #1

--- src/test/java/com/hexagonal/salarysystem/domain/PartTimeEmployeeTest.java
+++ src/test/java/com/hexagonal/salarysystem/domain/PartTimeEmployeeTest.java
@@ -9,10 +9,10 @@ public class PartTimeEmployeeTest {
     @Test
     void testCalculateSalaryWithSuccess() {
         double taxRate = 0.1;
-        int workTime = 20;
+        int workHour = 20, workMinute = 10;
         Money basePayOfTime = new Money(18);
 
-        Employee employee = new PartTimeEmployee(taxRate, new WorkDuration(workTime), basePayOfTime);
+        Employee employee = new PartTimeEmployee(taxRate, new WorkDuration(workHour, workMinute), basePayOfTime);
 
         Money expectedSalary = new Money(324); // 18 * 20 * (1 - 0.1) = 324
         assertEquals(expectedSalary, employee.calculateSalary(), "급여 계산이 올바르지 않습니다.");
